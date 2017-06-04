d23e24a7f606e2cd54eab6a10bf6a1d90ac6fed7
Junnosuke YOKOYAMA <jyokoyama@tsukinowa.jp>
2017-06-05 00:44:15 +0900

    ジャンプを追加

--- main.go
+++ main.go
@@ -142,21 +142,39 @@ func (g *Game) Init() {
 	g.Player.Position.Y = 10
 
 	// ブロック
-	block1 := sprite.NewBlock([]*ebiten.Image{blockImg})
-	block1.Position.X = 100
-	block1.Position.Y = 50
-	block2 := sprite.NewBlock([]*ebiten.Image{blockImg})
-	block2.Position.X = 200
-	block2.Position.Y = 100
-
-	g.Blocks = []*sprite.Block{
-		block1,
-		block2,
+	// 床
+	for x := 0; x < 320; x += 17 {
+		block := sprite.NewBlock([]*ebiten.Image{blockImg})
+		block.Position.X = x
+		block.Position.Y = 200
+		g.Blocks = append(g.Blocks, block)
 	}
+	// 第2床
+	for x := 8 * 17; x < 17*13; x += 17 {
+		block := sprite.NewBlock([]*ebiten.Image{blockImg})
+		block.Position.X = x
+		block.Position.Y = 110
+		g.Blocks = append(g.Blocks, block)
+	}
+
+	block1 := sprite.NewBlock([]*ebiten.Image{blockImg})
+	block1.Position.X = 60
+	block1.Position.Y = 160
+	g.Blocks = append(g.Blocks, block1)
+
+	block2 := sprite.NewBlock([]*ebiten.Image{blockImg})
+	block2.Position.X = 90
+	block2.Position.Y = 130
+	g.Blocks = append(g.Blocks, block2)
+
 }
 
 func (g *Game) MainLoop(screen *ebiten.Image) error {
-	g.Player.Move([]sprite.Sprite{g.Blocks[0], g.Blocks[1]})
+	sprites := []sprite.Sprite{}
+	for _, b := range g.Blocks {
+		sprites = append(sprites, b)
+	}
+	g.Player.Move(sprites)
 
 	if ebiten.IsRunningSlowly() {
 		return nil
--- sprite/player.go
+++ sprite/player.go
@@ -1,6 +1,15 @@
 package sprite
 
-import "github.com/hajimehoshi/ebiten"
+import (
+	"math"
+
+	"github.com/hajimehoshi/ebiten"
+)
+
+// 四捨五入関数
+func round(f float64) int {
+	return int(math.Floor(f + .5))
+}
 
 // isOverlap は x1-x2 の範囲の整数が x3-x4 の範囲と重なるかを判定する
 func isOverlap(x1, x2, x3, x4 int) bool {
@@ -12,13 +21,25 @@ func isOverlap(x1, x2, x3, x4 int) bool {
 
 type Player struct {
 	BaseSprite
+	jumping   bool    // 現在ジャンプ中か
+	jumpSpeed float64 // 現在のジャンプ力
+	fallSpeed float64 // 落下速度
 }
 
 func NewPlayer(images []*ebiten.Image) *Player {
 	player := new(Player)
 	player.Images = images
 	player.ImageNum = len(images)
+	player.jumpSpeed = 0
+	player.fallSpeed = 0.4
 	return player
+}
+
+func (p *Player) jump() {
+	if !p.jumping {
+		p.jumping = true
+		p.jumpSpeed = -6
+	}
 }
 
 func (p *Player) Move(objects []Sprite) {
@@ -33,13 +54,15 @@ func (p *Player) Move(objects []Sprite) {
 		p.count++
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyUp) {
-		dy = -1
+		p.jump()
 		p.count++
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyDown) {
-		dy = 1
-		p.count++
+
+	// 落下速度の計算
+	if p.jumpSpeed < 5 {
+		p.jumpSpeed += p.fallSpeed
 	}
+	dy = round(p.jumpSpeed)
 
 	for _, object := range objects {
 		dx, dy = p.IsCollide(dx, dy, object)
@@ -83,6 +106,10 @@ func (p *Player) IsCollide(dx, dy int, object Sprite) (int, int) {
 			// 下方向の移動の衝突判定
 			// 衝突していたらy軸の移動速度を 0 にする
 			dy = 0
+
+			// ジャンプ中フラグをオフにする
+			p.jumping = false
+			p.jumpSpeed = 0
 		}
 	}
 
